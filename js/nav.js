fc6e514086872490ba073df11d26926e4c93a7d1
Knightmaro <127098266+Knightmaro@users.noreply.github.com>
2024-12-22 23:15:23 +0800

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // 自动处理所有页面的导航链接，处理相对路径
 document.addEventListener('DOMContentLoaded', () => {
     // 获取当前页面的文件名
@@ -14,21 +13,4 @@ document.addEventListener('DOMContentLoaded', () => {
             link.classList.add('active');
         }
     });
-=======
-// 自动处理所有页面的导航链接，处理相对路径
-document.addEventListener('DOMContentLoaded', () => {
-    // 获取当前页面的文件名
-    const currentPath = window.location.pathname.split('/').pop();
-    
-    // 获取所有链接
-    document.querySelectorAll('a').forEach(link => {
-        // 获取链接的文件名
-        const href = link.getAttribute('href');
-        
-        // 如果链接匹配当前页面，添加active类
-        if (href === currentPath) {
-            link.classList.add('active');
-        }
-    });
->>>>>>> 541b86da41df02fd29c7ff8d407a48b535c0c876
 });
